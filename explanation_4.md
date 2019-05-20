03e8af7efd4ecd69de70b3e45d568699517bde12
Ryan Chu <tianyuan.chu@gmail.com>
2019-05-19 23:43:09 -0700

    partition

--- explanation_4.md
+++ explanation_4.md
@@ -1,1 +1,7 @@
 # Dutch National Flag Problem
+
+In order to sort array with single pass, we need to partition the array with three indexes, `curr_index` which keeps trace of 1, `zero_index` and `two_index`
+
+The goal is to try to keep all 0s to the left by leveraging the `zero_index`, and the same applies to 2s by using the `two_index`.
+
+The whole operation needs to iterate the array elements exactly once, so it is `O(n)`
--- problem_4.py
+++ problem_4.py
@@ -5,19 +5,57 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    pass
+    zero_index = 0
+    curr_index = 0
+    two_index = len(input_list)-1
+    while curr_index <= two_index:
+        curr_val = input_list[curr_index]
+        # 0
+        if curr_val < 1:
+            # swap zero to the left
+            if zero_index < curr_index:
+                input_list[curr_index], input_list[zero_index] = input_list[zero_index], input_list[curr_index]
+
+            zero_index += 1
+            curr_index += 1
+            continue
+        # 2
+        if curr_val > 1:
+            if input_list[two_index] > 1:
+                two_index -= 1
+                continue
+            else:
+                # swap two to the right
+                input_list[curr_index], input_list[two_index] = input_list[two_index], input_list[curr_index]
+                two_index -= 1
+                continue
+        # 1
+        else:
+            # just move
+            curr_index += 1
+    return input_list
 
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
-    print(sorted_array)
     if sorted_array == sorted(test_case):
         print("Pass")
     else:
         print("Fail")
 
 
+# empty and edge case
+test_function([])
+test_function([0, 0])
+test_function([1, 1])
+test_function([2, 2])
+test_function([2, 1])
+
+# random cases
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2,
                2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+
+# sorted and reverse cases
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([2, 2, 2, 1, 1, 1, 0, 0, 0])
