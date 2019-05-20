89c9550310bbd705a849804f72dffd901cfbeeba
Ryan Chu <tianyuan.chu@gmail.com>
2019-05-19 22:06:28 -0700

    initial

---
+++ explanation_3.md
@@ -0,0 +1,7 @@
+# Rearrange Array Elements
+
+Given that array elements are from 0 to 9, we don't need to worry about double digits.
+
+Simply sort the array, not matter there are duplicates or not, it shouldn't matter, and fill the two array from most significant digit to the least, one by one, until the array runs out.
+
+The sorting itself will take `O(nLogN)` and the filling process will take `O(n)`, and thus the overall complexity will be `O(nLogN)`
--- problem_3.py
+++ problem_3.py
@@ -7,7 +7,18 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    return 0, 0
+    # Sort
+    input_list.sort(reverse=True)
+    # Fill the digits
+    first = "0"
+    second = "0"
+    for index, digit in enumerate(input_list):
+        if index % 2 == 0:
+            first += str(digit)
+        else:
+            second += str(digit)
+
+    return int(first), int(second)
 
 
 def test_function(test_case):
@@ -19,5 +30,16 @@ def test_function(test_case):
         print("Fail")
 
 
+# edge cases
+test_function([[], [0, 0]])
+test_function([[1], [1, 0]])
+test_function([[1, 2], [2, 1]])
+
+# with duplicates
+test_function([[2, 2, 2, 2], [22, 22]])
+test_function([[2, 2, 2, 2, 2], [222, 22]])
+
+# normal cases
+test_function([[1, 2, 4, 5], [52, 41]])
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
