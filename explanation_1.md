128d7c6ca7203987f9f2d7010ead85fa37bd82d3
Ryan Chu <tianyuan.chu@gmail.com>
2019-06-02 14:12:24 -0700

    problem 1 amends

--- explanation_1.md
+++ explanation_1.md
@@ -2,4 +2,6 @@
 
 It is implemented with an variation of binary search, and it will stop until the lower or upper bound meets the middle point.
 
-Due to the nature of the binary search, the complexity is `O(n)`
+Due to the nature of the binary search, the complexity is `O(log(n))`
+
+In terms of the space complexity, since we are keeping track of `high`, `low` and `mid` indexes and not relevant to the size of the input, it's `O(1)`
--- problem_1.py
+++ problem_1.py
@@ -18,19 +18,26 @@ def sqrt(number):
             break
         elif mid * mid > number:
             high = mid
-            mid = math.floor((low + mid) / 2)
+            mid = (low + mid) // 2
         else:
             low = mid
-            mid = math.floor((mid + high) / 2)
+            mid = (mid + high) // 2
     return mid
 
 
+# base cases
 print("Pass" if (0 == sqrt(0)) else "Fail")
 print("Pass" if (1 == sqrt(1)) else "Fail")
 print("Pass" if (1 == sqrt(2)) else "Fail")
+
+# complete squares
 print("Pass" if (2 == sqrt(4)) else "Fail")
 print("Pass" if (3 == sqrt(9)) else "Fail")
 print("Pass" if (4 == sqrt(16)) else "Fail")
+
+# normal cases
 print("Pass" if (5 == sqrt(27)) else "Fail")
 print("Pass" if (10 == sqrt(101)) else "Fail")
+
+# large inputs
 print("Pass" if (56941 == sqrt(3242343667)) else "Fail")
